a445881ed8d7017d265e6e71249c1340d02f1563
dawn1810 <binhminh19112003@gmail.com>
2024-02-28 12:54:40 +0700

    custom player part 1

--- wtfmovies/src/pages/Watch/Watch.js
+++ wtfmovies/src/pages/Watch/Watch.js
@@ -1,4 +1,3 @@
-import ReactPlayer from 'react-player/youtube';
 import classNames from 'classnames/bind';
 
 import FilmInfo from '~/components/FilmInfo/FilmInfo';
@@ -192,7 +191,7 @@ function Watch() {
         <div className={cx('wrapper')}>
             <h1 className={cx('title')}>CÔ GÁI ĐẾN TỪ TƯƠNG LAI</h1>
             <TabsBox tabs={notyfyTabs} textContent defaultActiveKey="celender" className={cx('tab-box')} />
-            <Player url={'https://rurimeiko.pages.dev/demo3.m3u8'}/>
+            <Player url={'https://rurimeiko.pages.dev/demo3.m3u8'} />
             <FilmInteract />
             <TabsBox tabs={episodesTabs} flexContent textContent defaultActiveKey="visub" className={cx('tab-box')} />
             <TabsBox
