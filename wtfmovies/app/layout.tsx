2431ba58e0e21d3cd0be7db392d39fa568688fa2
dawn1810 <binhminh19112003@gmail.com>
2024-03-18 00:28:10 +0700

    font classname for layout.tsx

--- wtfmovies/app/layout.tsx
+++ wtfmovies/app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from "next";
-import "./globals.scss";
+import './globals.scss';
+import type { Metadata } from 'next';
+import { montserrat } from './font';
 
 export const metadata: Metadata = {
-  title: "WTF movies",
-  description: "Nơi thoả sức đam mê phim ảnh",
+    title: 'WTF movies',
+    description: 'Nơi thoả sức đam mê phim ảnh',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={montserrat.className}>{children}</body>
+        </html>
+    );
 }
