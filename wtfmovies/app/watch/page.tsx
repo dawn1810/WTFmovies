14514955e8cdb8913f01174985cb676ff01bae98
Ruri Meiko <chandoralong@gmail.com>
2024-04-08 22:54:30 +0700

    fix video player

--- wtfmovies/app/watch/page.tsx
+++ wtfmovies/app/watch/page.tsx
@@ -195,7 +195,7 @@ async function Watch() {
             <div className={cx('wrapper')}>
                 <h1 className={cx('title')}>Inuyashiki (いぬやしき)</h1>
                 <TabsBox tabs={notyfyTabs} textContent defaultActiveKey="celender" className={cx('tab-box')} />
-                <Player url={'https://p21-ad-sg.ibyteimg.com/obj/ad-site-i18n-sg/202404075d0ddd9fdb8ed672411ea059?.m3u8'} />
+                <Player url={'https://p21-ad-sg.ibyteimg.com/obj/ad-site-i18n-sg/202404085d0dd7ded2f8d41549d4b97a?.m3u8'} />
                 <FilmInteract />
                 <TabsBox
                     tabs={episodesTabs}
