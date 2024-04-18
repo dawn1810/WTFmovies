1471004a1572715abbdb462a1c2a051033ff143b
Ruri Meiko <chandoralong@gmail.com>
2024-04-18 13:48:20 +0700

    fix metadata

--- wtfmovies/app/layout.tsx
+++ wtfmovies/app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
     },
     description: APP_DESCRIPTION,
     manifest: "/manifest.json",
+    icons: '/images/icons/favicon.svg',
     appleWebApp: {
         capable: true,
         statusBarStyle: "default",
@@ -28,23 +29,23 @@ export const metadata: Metadata = {
     formatDetection: {
         telephone: false,
     },
-    openGraph: {
-        type: "website",
-        siteName: APP_NAME,
-        title: {
-            default: APP_DEFAULT_TITLE,
-            template: APP_TITLE_TEMPLATE,
-        },
-        description: APP_DESCRIPTION,
-    },
-    twitter: {
-        card: "summary",
-        title: {
-            default: APP_DEFAULT_TITLE,
-            template: APP_TITLE_TEMPLATE,
-        },
-        description: APP_DESCRIPTION,
-    },
+    // openGraph: {
+    //     type: "website",
+    //     siteName: APP_NAME,
+    //     title: {
+    //         default: APP_DEFAULT_TITLE,
+    //         template: APP_TITLE_TEMPLATE,
+    //     },
+    //     description: APP_DESCRIPTION,
+    // },
+    // twitter: {
+    //     card: "summary",
+    //     title: {
+    //         default: APP_DEFAULT_TITLE,
+    //         template: APP_TITLE_TEMPLATE,
+    //     },
+    //     description: APP_DESCRIPTION,
+    // },
 };
 
 export const viewport: Viewport = {
--- wtfmovies/next.config.mjs
+++ wtfmovies/next.config.mjs
@@ -10,7 +10,7 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: true,
+  disable: false,
   cacheOnFrontendNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
