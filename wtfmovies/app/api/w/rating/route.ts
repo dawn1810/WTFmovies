2e7b936c394e13b0312ea300ab3f3791b802fdbb
Ruri Meiko <chandoralong@gmail.com>
2024-04-20 21:20:21 +0700

    eto

--- wtfmovies/app/api/w/rating/route.ts
+++ wtfmovies/app/api/w/rating/route.ts
@@ -1,25 +1,21 @@
 export const runtime = 'edge';
 import type { NextRequest } from 'next/server';
 import { ObjectId, mongodb, toError, toJSON } from '~/libs/func';
-import { ExtendedUser, MongoUpdate } from '~/libs/interfaces';
+import { MongoUpdate } from '~/libs/interfaces';
 import { auth } from '../../auth/[...nextauth]/auth';
+import { User } from 'next-auth';
 
 export async function POST(request: NextRequest) {
     const data = await request.json() as any;
     const { epId, rating, userId } = data;
     const session = await auth();
-
-    if (!session) return new Response(JSON.stringify({ 'statusCode': 200, 'content': 'ok' }, null, 2));
-
-
-    const extendedUser: ExtendedUser | undefined = session?.user;
-
+    if (!session || !session.user || !session.user.id) return toError({ status: 403, content: "missing data" }, 200);
     if (!epId || !rating || !userId) return toError({ status: 403, content: "missing data" }, 200);
     const updateRes: MongoUpdate = await mongodb()
         .db('film')
         .collection('rating')
         .updateOne({
-            filter: { id_ep: ObjectId(epId), id_user: ObjectId(extendedUser?.user_id) },
+            filter: { id_ep: ObjectId(epId), id_user: ObjectId(session.user.id) },
             update: {
                 "$set": {
                     rating: { "$numberDouble": String(rating) }
