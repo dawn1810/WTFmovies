02043f1dadc672bf288b0715debcf33d1f3a9cbc
Ruri Meiko <chandoralong@gmail.com>
2024-04-21 15:54:57 +0700

    doing search

--- wtfmovies/app/api/test/route.ts
+++ wtfmovies/app/api/test/route.ts
@@ -1,54 +1,27 @@
-import { mongodb } from '~/libs/func';
+import { ObjectId, mongodb, toJSON } from '~/libs/func';
 export const runtime = 'edge';
 import type { NextRequest } from 'next/server';
 
 
 export async function GET(request: NextRequest) {
+    const data = await mongodb().db('film').collection('genre').find({ filter: {} })
 
-    // return new Response(JSON.stringify(await mongodb().db('film').collection('information').updateMany({
-    //     filter: {},
-    //     update: {
-    //         "$unset": {
-    //             rating: 0
-    //         }
-    //     }
-    // }), null, 2));
-    return new Response(JSON.stringify(await mongodb()
-        .db('film')
-        .collection('episode')
-        .aggregate({
-            pipeline: [
-                {
-                    $match: {
-                        film_id:
-                            "bfae10e9-d0c9-4746-9be9-a6ea16f369b1"
-                    }
-                }
-                ,
-                {
-                    $lookup: {
-                        from: 'rating',
-                        localField: '_id',
-                        foreignField: 'id_ep',
-                        as: 'reviews',
-                    },
-                },
-                {
-                    $project: {
-                        _id: 1,
-                        film_id: 1,
-                        index: 1,
-                        name: 1,
-                        uploader_id: 1,
-                        upload_date: 1,
-                        rating: { $round: [{ $avg: '$reviews.rating' }, 1] },
-                        views: 1,
-                        link: 1
-                    },
+    let res: any = [];
+    data.forEach(async doc => {
 
-                },
-                { $merge: { into: "episode", on: "rating", whenMatched: "replace", whenNotMatched: "insert" } }]
-        }), null, 2));
+        const name = doc.name.toLowerCase().replace(/\s+/g, '-');
+        res.push(await mongodb().db('film').collection('genre').updateOne(
+            {
+                filter:
+                    { _id: ObjectId(doc._id) },
+                update:
+                    { $set: { searchName: name } }
+            }
+        ));
+        console.log('>>', res);
+    })
+    return toJSON(res);
+
 
 
 
