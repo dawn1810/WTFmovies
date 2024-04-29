b9fb03edd676ff34ad461849751dd67c1e9d04c9
dawn1810 <binhminh19112003@gmail.com>
2024-04-29 17:21:22 +0700

    admin ui

--- wtfmovies/components/AdminDashboard/PieCard/PieCard.tsx
+++ wtfmovies/components/AdminDashboard/PieCard/PieCard.tsx
@@ -7,9 +7,11 @@ import style from './PieCard.module.scss';
 const cx = classNames.bind(style);
 
 const data = [
-    { id: 0, value: 10, label: 'series A' },
-    { id: 1, value: 15, label: 'series B' },
-    { id: 2, value: 20, label: 'series C' },
+    { id: 0, value: 100, label: 'Naruto' },
+    { id: 1, value: 75, label: 'Sasuke' },
+    { id: 2, value: 70, label: '寂しい 犬' },
+    { id: 3, value: 50, label: 'Lon Dai dE' },
+    { id: 4, value: 18, label: 'Đẹp gái nhất thế giới' },
 ];
 
 export default function PieCard({ area }: { area: string }) {
@@ -28,14 +30,18 @@ export default function PieCard({ area }: { area: string }) {
                             data,
                             highlightScope: { faded: 'global', highlighted: 'item' },
                             faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
-                            cx: '65%',
-                            cy: '45%',
+                            startAngle: 90,
+                            endAngle: 360,
+                            // cx: '65%',
+                            // cy: '45%',
                         },
                     ]}
                     slotProps={{
-                        legend: { position: { horizontal: 'middle', vertical: 'bottom' }, direction: 'row' },
+                        legend: {
+                            position: { horizontal: 'right', vertical: 'top' },
+                        },
                     }}
-                    height={400}
+                    height={350}
                 />
             </CardActionArea>
         </Card>
