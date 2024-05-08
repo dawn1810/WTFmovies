748c414072ad55b5fb887f862b56d518b9bcdc1f
Ruri Meiko <chandoralong@gmail.com>
2024-05-08 18:20:56 +0700

    render editor

--- wtfmovies/app/(editor)/editor/[[...page]]/film.tsx
+++ wtfmovies/app/(editor)/editor/[[...page]]/film.tsx
@@ -1,22 +1,24 @@
-'use client';
+'use client'
 import FilmManager from '~/components/FilmManager';
 import { data } from './mockdata';
+import { getFilm } from '~/libs/getData/editor';
 
 export const colum = [
-    { headerName: 'Phim ID', field: 'id', width: 280 },
-    { headerName: 'Tên phim', field: 'name', width: 150 },
+    { headerName: 'Phim ID', field: 'id', width: 190 },
+    { headerName: 'Tên phim', field: 'name', width: 250 },
     { headerName: 'Mô tả', field: 'describe', width: 100 },
     { headerName: 'Trạng thái', field: 'status', width: 100 },
     { headerName: 'Tác giả', field: 'author', width: 100 },
     { headerName: 'Thể loại', field: 'gerne', width: 100 },
     { headerName: 'Đạo diễn', field: 'director', width: 100 },
     { headerName: 'Thời lượng', field: 'duration', width: 100 },
+    { headerName: 'Tổng số tập', field: 'maxEp', width: 100 },
     { headerName: 'Loại video', field: 'videoType', width: 100 },
     { headerName: 'Năm xuất bản', field: 'releaseYear', width: 100 },
     { headerName: 'Quốc gia', field: 'country', width: 100 },
     { headerName: 'Điểm đánh giá', field: 'rating', width: 100 },
 ];
-export default function FilmPage() {
+export default function FilmPage({ data }: { data: any }) {
     return (
         <FilmManager title_name="Tổng quan" colum={colum}>
             {data}
--- wtfmovies/libs/getData/editor.ts
+++ wtfmovies/libs/getData/editor.ts
@@ -3,12 +3,67 @@ import { mongodb } from '~/libs/func';
 
 export const getFilm = async (): Promise<FilmInfo[]> => {
     try {
-        const filmList: FilmInfo[] = await mongodb()
+        const films: any[] = await mongodb()
             .db('film')
             .collection('information')
             .aggregate({
                 pipeline: [
-                    { $sort: { weekViews: -1, likes: -1 /* add rating hear too how ??? */ } },
+                    { $match: {} },
+                    {
+                        $lookup: {
+                            from: 'author',
+                            let: { authorIds: '$author' }, // Define the local variable authorIds
+                            pipeline: [
+                                { $match: { $expr: { $in: ['$_id', '$$authorIds'] } } }, // Match the author ids
+                                { $project: { _id: 0, name: 1 } }, // Get name only
+                            ],
+                            as: 'authorDetails',
+                        },
+                    },
+                    {
+                        $lookup: {
+                            from: 'genre',
+                            let: { genreIds: '$genre' }, // Define the local variable genreIds
+                            pipeline: [
+                                { $match: { $expr: { $in: ['$_id', '$$genreIds'] } } }, // Match the genre ids
+                                { $project: { _id: 0, name: 1 } }, // Get name only
+                            ],
+                            as: 'genreDetails',
+                        },
+                    },
+                    {
+                        $lookup: {
+                            from: 'director',
+                            let: { directorIds: '$director' }, // Define the local variable genreIds
+                            pipeline: [
+                                { $match: { $expr: { $in: ['$_id', '$$directorIds'] } } }, // Match the genre ids
+                                { $project: { _id: 0, name: 1 } }, // Get name only
+                            ],
+                            as: 'directorDetails',
+                        },
+                    },
+                    {
+                        $lookup: {
+                            from: 'actor',
+                            let: { actorIds: '$actor' }, // Define the local variable genreIds
+                            pipeline: [
+                                { $match: { $expr: { $in: ['$_id', '$$actorIds'] } } }, // Match the genre ids
+                                { $project: { _id: 0, name: 1 } }, // Get name only
+                            ],
+                            as: 'actorDetails',
+                        },
+                    },
+                    {
+                        $lookup: {
+                            from: 'tag',
+                            let: { tagIds: '$tag' }, // Define the local variable genreIds
+                            pipeline: [
+                                { $match: { $expr: { $in: ['$_id', '$$tagIds'] } } }, // Match the genre ids
+                                { $project: { _id: 0, name: 1 } }, // Get name only
+                            ],
+                            as: 'tagDetails',
+                        },
+                    },
                     {
                         $lookup: {
                             from: 'episode',
@@ -19,20 +74,36 @@ export const getFilm = async (): Promise<FilmInfo[]> => {
                     },
                     {
                         $project: {
-                            _id: 0,
+                            _id: 1,
                             name: 1,
-                            views: '$weekViews',
-                            likes: 1,
+                            searchName: 1,
+                            describe: 1,
+                            author: '$authorDetails.name',
+                            genre: '$genreDetails.name',
+                            director: '$directorDetails.name',
+                            videoType: { $arrayElemAt: [{ $arrayElemAt: ['$videoType.episode', 0] }, -1] },
+                            views: 1,
+                            maxEp: 1,
                             rating: { $round: [{ $avg: '$reviews.rating' }, 1] },
+                            img: 1,
+                            status: 1,
+                            duration: 1,
+                            tag: '$tagDetails.name',
+                            releaseYear: 1,
+                            country: 1,
+                            actor: '$actorDetails.name',
                         },
                     },
-                    { $limit: 5 },
                 ],
             });
 
-        return filmList;
+
+
+        return films.map(film => {
+            return { ...film, id: film._id, _id: undefined };
+        });
     } catch (err) {
-        console.log('😨😨😨 error at admin/getTopHotFilm function : ', err);
+        console.log('😨😨😨 error at editor/getFilm function : ', err);
         return [];
     }
 };
