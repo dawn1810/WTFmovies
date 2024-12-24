a5eac08caf3decde1136d067db9b1ea7f475acb4
Ruri Meiko <chandoralong@gmail.com>
2024-12-25 00:09:30 +0700

    cover ws to cloudflare

---
+++ wtfmovies-cloudflare/app/api/v1/ws/route.ts
@@ -0,0 +1,20 @@
+
+export const runtime = 'edge';
+import { NextRequest } from "next/server";
+
+export function SOCKET(
+    client: import('ws').WebSocket,
+    request: import('http').IncomingMessage,
+    server: import('ws').WebSocketServer
+) {
+    console.log('A client connected');
+
+    client.on('message', (message) => {
+        console.log('Received message:', message);
+        client.send(message);
+    });
+
+    client.on('close', () => {
+        console.log('A client disconnected');
+    });
+}
